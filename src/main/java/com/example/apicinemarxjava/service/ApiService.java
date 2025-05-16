8922ed469b04978cdf8f68cfd7347054aa97999d
dev <dev@MacBook-Pro-de-Javier.local>
2025-05-16 10:36:38 +0200

    añadido service

--- src/main/java/com/example/apicinemarxjava/service/ApiService.java
+++ src/main/java/com/example/apicinemarxjava/service/ApiService.java
@@ -1,6 +1,55 @@
 package com.example.apicinemarxjava.service;
 
+
+import com.example.apicinemarxjava.model.Pelicula;
+import com.example.apicinemarxjava.model.Proyeccion;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import hu.akarnokd.rxjava3.retrofit.RxJava3CallAdapterFactory;
+import io.reactivex.rxjava3.core.Observable;
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
 public class ApiService {
+
+ private String BASE_URL = "http://localhost:8080";
+ private ApiCinema apiCinema;
+
+ public ApiService() {
+     HttpLoggingInterceptor logging = new HttpLoggingInterceptor();
+     logging.setLevel(HttpLoggingInterceptor.Level.BODY);
+     OkHttpClient client = new OkHttpClient.Builder()
+             .addInterceptor(logging)
+             .build();
+
+     Gson gson = new GsonBuilder()
+             .setLenient()
+             .create();
+
+     Retrofit retrofit = new Retrofit.Builder()
+             .baseUrl(BASE_URL)
+             .client(client)
+             .addConverterFactory(GsonConverterFactory.create(gson))
+             .addCallAdapterFactory(RxJava3CallAdapterFactory.create())
+             .build();
+
+
+     this.apiCinema = retrofit.create(ApiCinema.class);
+ }
+
+ public Observable<Pelicula> getAllPeliculas() {
+
+     return apiCinema.getAllPeliculas()
+             .flatMapIterable(pelicula -> pelicula);
+ }
+
+ public Observable<Proyeccion> getAllProyecciones() {
+     return apiCinema.getAllProyecciones()
+             .flatMapIterable(proyeccion -> proyeccion);
+ }
+
 
 
 
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,14 @@
 module com.example.apicinemarxjava {
     requires javafx.controls;
     requires javafx.fxml;
+    requires okhttp3.logging;
+    requires okhttp3;
+    requires com.google.gson;
+    requires retrofit2;
+    requires retrofit2.converter.gson;
+    requires rxjava3.retrofit.adapter;
+    requires io.reactivex.rxjava3;
+    requires static lombok;
 
 
     opens com.example.apicinemarxjava to javafx.fxml;
